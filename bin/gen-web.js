9b0616aa75711093364802aabaae669f48acb46e
Lee <dleeknowles@gmail.com>
2025-11-24 08:22:28 -0500

    feat: add --version flag

--- bin/gen-web.js
+++ bin/gen-web.js
@@ -6,6 +6,12 @@ const { copyTemplate } = require('../src/copyTemplate');
 function run() {
   const args = process.argv.slice(2);
 
+  if (args.includes("--version") || args.includes("-v")) {
+    const pkg = require("../package.json");
+    console.log(pkg.version);
+    return;
+  }
+
   if (args.length === 0) {
     console.error('Usage: gen-web <project-name>');
     process.exit(1);
